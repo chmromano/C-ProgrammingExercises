099b7369b0c8f583612705e7ee5fccf965f7f738
Christopher Romano <christopher.romano@icloud.com>
2021-09-07 16:45:33 +0300

    Added EX8 and EX9

---
+++ Exercise_9/cmake-build-debug/CMakeFiles/Exercise_9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise_9.dir/main.c.o"
+  "Exercise_9"
+  "Exercise_9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Exercise_9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Exercise_9/cmake-build-debug/CMakeFiles/Exercise_9.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Exercise_9.
+# This may be replaced when dependencies are built.
