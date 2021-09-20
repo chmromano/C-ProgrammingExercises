4761dd7b3964b7c9a9e0c7eeb127346c472a2d5c
Christopher Romano <christopher.romano@icloud.com>
2021-09-20 21:01:27 +0300

    EX11 finished. Only improvement would be DMA.

--- Exercise_11/main.c
+++ Exercise_11/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
 //To sort more than 3 numbers just change this
 #define TO_READ 3
+#define STRING_LENGTH 102
+
+#define STRING_INTEGERS "1234567890"
+
+bool read_int(int *input, FILE *file);
 
 int main() {
+    bool successful_execution = true;
+
     int unsorted_array[TO_READ] = {};
     int sorted_array[TO_READ] = {};
 
@@ -19,38 +29,69 @@ int main() {
         printf("\nThere was an error opening the file.\n");
     } else {
         for (int i = 0; i < TO_READ; i++) {
-            fscanf(my_file, "%d", &unsorted_array[i]);
-        }
-
-        for (int i = 0; i < TO_READ; i++) {
-
-            smallest = unsorted_array[0];
-            smallest_index = 0;
-
-            for (int j = 0; j < unsorted_array_size; j++) {
-                if (unsorted_array[j] < smallest) {
-                    smallest = unsorted_array[j];
-                    smallest_index = j;
-                }
-            }
-
-            unsorted_array_size--;
-            sorted_array[i] = smallest;
-
-            for (int k = smallest_index; k <= unsorted_array_size; k++) {
-                if (smallest_index != unsorted_array_size) {
-                    unsorted_array[k] = unsorted_array[k + 1];
-                }
+            if (read_int(&unsorted_array[i], my_file) == false) {
+                printf("\nThere was an error while reading the numbers.\n");
+                successful_execution = false;
+                break;
             }
         }
 
-        fclose(my_file);
-        my_file = fopen("numbers.txt", "w");
+        if (successful_execution == true) {
+            for (int i = 0; i < TO_READ; i++) {
 
-        for (int i = 0; i < TO_READ; i++) {
-            fprintf(my_file, "%d\n", sorted_array[i]);
+                smallest = unsorted_array[0];
+                smallest_index = 0;
+
+                for (int j = 0; j < unsorted_array_size; j++) {
+                    if (unsorted_array[j] < smallest) {
+                        smallest = unsorted_array[j];
+                        smallest_index = j;
+                    }
+                }
+
+                unsorted_array_size--;
+                sorted_array[i] = smallest;
+
+                for (int k = smallest_index; k <= unsorted_array_size; k++) {
+                    if (smallest_index != unsorted_array_size) {
+                        unsorted_array[k] = unsorted_array[k + 1];
+                    }
+                }
+            }
+
+            my_file = fopen("numbers.txt", "w");
+
+            for (int i = 0; i < TO_READ; i++) {
+                fprintf(my_file, "%d\n", sorted_array[i]);
+            }
         }
     }
 
     return 0;
+}
+
+bool read_int(int *input, FILE *file) {
+
+    bool read_success = true;
+
+    char temp_string[STRING_LENGTH] = {};
+
+    if (fgets(temp_string, STRING_LENGTH, file) == NULL) {
+        read_success = false;
+    } else {
+        if (temp_string[strlen(temp_string) - 1] == '\n') {
+            temp_string[strlen(temp_string) - 1] = '\0';
+        }
+    }
+
+    for (int i = 0; i < strlen(temp_string); i++) {
+        if (strchr(STRING_INTEGERS, temp_string[i]) == NULL) {
+            read_success = false;
+            break;
+        }
+    }
+
+    *input = atoi(temp_string);
+
+    return read_success;
 }
