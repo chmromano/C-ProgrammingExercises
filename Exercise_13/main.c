312b9722fd6c82fe18b22c3c82d314cfbd707f66
Christopher Romano <christopher.romano@icloud.com>
2021-09-26 18:01:03 +0300

    EX12 finished

--- Exercise_13/main.c
+++ Exercise_13/main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define FILE_NAME "strings.txt"
 
 int main() {
-    printf("Hello, World!\n");
+
+    bool run_program = true;
+
+    FILE *my_file;
+    my_file = fopen(FILE_NAME, "w");
+
+    if (my_file == NULL) {
+        printf("There was an error opening the file.");
+    } else {
+        while (run_program == true) {
+
+            bool read_string = true;
+
+            char string[] = {};
+            int string_length = 0;
+
+            while (read_string == true) {
+                int character;
+                character = getchar();
+
+                if (character == '\n') {
+                    read_string = false;
+                } else {
+                    string_length++;
+                }
+            }
+
+            if (strlen(string) == 1 && string[0] == '.') {
+                run_program = false;
+            } else {
+                fprintf(my_file, "%d:%s", string_length, string);
+            }
+        }
+    }
+
+    fclose(my_file);
     return 0;
 }
--- Test_code/main.c
+++ Test_code/main.c
@@ -1,47 +1,36 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define STRING_SIZE 20
-
-void merge_whitespace(char *string);
+#define FILE_NAME "test_file"
 
 int main() {
-    char string1[] = "   This  time with   \t\tleading spaces. \tOne\n"
-                     "\n"
-                     " more for the road ";
-    char string2[] = "    we live  in     Helsinki";
-    char string3[] = "i      go back to Switzerland    \n\nin   October";
 
-    merge_whitespace(string1);
-    merge_whitespace(string2);
-    merge_whitespace(string3);
+    bool run_program = true;
 
-    printf("%s\n%s\n%s", string1, string2, string3);
+    int char_count = 0;
 
-    return 0;
-}
+    int character;
 
-void merge_whitespace(char *string) {
-    int i = 0;
-    int length = strlen(string);
+    FILE *my_file;
+    my_file = fopen(FILE_NAME, "w");
 
-    bool first_space_found = false;
+    if (my_file == NULL) {
+        printf("There was an error opening the file.");
+    } else {
+        while (run_program == true) {
 
-    while (i < length) {
-        if ((string[i] == 9 || string[i] == 10 || string[i] == 11 || string[i] == 12 || string[i] == 13 || string[i] == 32)
-            && first_space_found == false) {
-            first_space_found = true;
-            i++;
-        } else if ((string[i] == 9 || string[i] == 10 || string[i] == 11 || string[i] == 12 || string[i] == 13 || string[i] == 32)
-                   && first_space_found == true) {
-            for (int j = i; j < length; j++) {
-                string[j] = string[j + 1];
+            character = getchar();
+
+            if (character != '.' && character != '\n') {
+                char_count++;
+                fprintf(my_file, "%d:%c\n", char_count, character);
+            } else if (character == '.') {
+                run_program = false;
             }
-        } else if ((string[i] != 9 || string[i] != 10 || string[i] != 11 || string[i] != 12 || string[i] != 13 || string[i] != 32)) {
-            first_space_found = false;
-            i++;
         }
     }
+
+    fclose(my_file);
+    return 0;
 }
