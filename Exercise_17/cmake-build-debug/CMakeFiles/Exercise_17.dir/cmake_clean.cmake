824f8f821fbd79a9dd1b9d39f0271652f2af75d9
Christopher Romano <christopher.romano@icloud.com>
2021-10-05 18:19:07 +0300

    Started EX17

---
+++ Exercise_17/cmake-build-debug/CMakeFiles/Exercise_17.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise_17.dir/main.c.o"
+  "Exercise_17"
+  "Exercise_17.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Exercise_17.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Exercise_17/cmake-build-debug/CMakeFiles/Exercise_17.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Exercise_17.
+# This may be replaced when dependencies are built.
