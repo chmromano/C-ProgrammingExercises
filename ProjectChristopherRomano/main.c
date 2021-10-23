7a5cb226a70d0ecd3a3cfbacc2d7ad0c1eb80b75
Christopher Romano <christopher.romano@icloud.com>
2021-10-23 14:03:26 +0200

    Trying to fix printing

--- ProjectChristopherRomano/main.c
+++ ProjectChristopherRomano/main.c
@@ -7,7 +7,26 @@
 #define ADDR "Address"
 #define PWD "Password"
 
-#define OPTIONS_PROMPT "Choose an option:\n1. Open existing file\n2. Create new file\n3. Quit\n"
+#define PRINT_ALL (char *) "*\0"
+#define READ_BIN "rb"
+#define WRITE_BIN "wb"
+
+#define OPTIONS_PROMPT "\nWelcome. Available options:\n\n1. Open existing file\n2. Create new file\n3. "\
+"Quit\n\nChoose an option: "
+#define INVALID_INT_MAIN_MSG "Invalid input. Enter a valid choice (1-3): "
+#define INVALID_CHOICE_MAIN_MSG "Invalid choice. Enter a valid choice (1-3): "
+#define FILE_OPEN_MSG "\nEnter file to open: "
+#define FILE_CREATE_MSG "\nCreating file. Enter file name: "
+#define EMPTY_STRING_MSG "\nString cannot be empty. Enter a valid string: "
+#define QUIT_MSG "\nQuitting program...\n"
+
+#define ENTRY_OPERATIONS_PROMPT "\nAvailable operations:\n\n1. New entry\n2. Search entry by address\n3. Print entries "\
+"report\n4. Delete entry by address\n5. Save and exit\n6. Exit without saving\n\nChoose an operation: "
+#define INVALID_INT_COMMAND_MSG "Invalid input. Enter a valid choice (1-6): "
+#define INVALID_CHOICE_COMMAND_MSG "Invalid choice. Enter a valid choice (1-6): "
+
+#define NO_PWD_MSG "\nNo passwords stored.\n"
+
 #define FILE_ERROR_MSG printf("There was an error opening the file.\n")
 #define MEM_CHECK(x) if(x == NULL){printf("There was an error allocating memory. Quitting program...");exit(1);}
 
@@ -22,11 +41,9 @@ typedef enum {
     NO_OPTION, OPEN_EXISTING, CREATE_NEW, QUIT
 } OPTION;
 
-ENTRY *init_entries();
-
-OPTION option_select(char *file_name, FILE **file);
-
 ENTRY *new_entry(ENTRY *entry_array, int *array_len);
+
+char *decrypt(char *pwd, int pwd_len, char *master);
 
 void print_entries(char *addr, ENTRY *entry_array, int array_len);
 
@@ -39,143 +56,104 @@ bool input_integer(int *input);
 char *input_string();
 
 int main() {
-
     OPTION chosen_option = NO_OPTION;
-
     while (chosen_option != QUIT) {
-
         ENTRY *entry_array;
-        FILE *working_file = NULL;
-        printf("Welcome. %s", OPTIONS_PROMPT);
-
+        entry_array = NULL;
         int array_len = 0;
 
         char *file_name;
+        FILE *working_file = NULL;
+
+        printf(OPTIONS_PROMPT);
 
         while (chosen_option == NO_OPTION) {
 
             int choice_main;
-            while (input_integer(&choice_main) == false) printf("Invalid characters.\n");
+            while (input_integer(&choice_main) == false) printf(INVALID_INT_MAIN_MSG);
 
             switch (choice_main) {
                 case 1:
+                    printf(FILE_OPEN_MSG);
                     file_name = input_string();
-                    working_file = fopen(file_name, "rb");
+                    working_file = fopen(file_name, READ_BIN);
                     if (working_file == NULL) {
                         FILE_ERROR_MSG;
                     } else {
                         chosen_option = OPEN_EXISTING;
                         entry_array = read_entries(&array_len, working_file);
                         fclose(working_file);
-                        working_file = fopen(file_name, "wb");
-                        if (working_file == NULL) {
-                            FILE_ERROR_MSG;
-                            chosen_option = NO_OPTION;
-                        }
                     }
-                    free(file_name);
                     break;
                 case 2:
+                    printf(FILE_CREATE_MSG);
                     file_name = input_string();
-                    working_file = fopen(file_name, "wb");
+                    working_file = fopen(file_name, WRITE_BIN);
                     if (working_file == NULL) {
                         FILE_ERROR_MSG;
                     } else {
                         chosen_option = CREATE_NEW;
-                        entry_array = init_entries();
                     }
-                    free(file_name);
+                    array_len = 0;
+                    entry_array = malloc(sizeof(ENTRY));
+                    MEM_CHECK(entry_array)
                     break;
                 case 3:
                     chosen_option = QUIT;
-                    printf("Quitting program...");
+                    printf(QUIT_MSG);
                     break;
                 default:
-                    printf("Invalid choice.\n");
+                    printf(INVALID_CHOICE_MAIN_MSG);
                     break;
             }
         }
 
-        //chosen_option = option_select(file_name, &working_file);
+        if (chosen_option == CREATE_NEW || chosen_option == OPEN_EXISTING) printf(ENTRY_OPERATIONS_PROMPT);
 
         while (chosen_option == CREATE_NEW || chosen_option == OPEN_EXISTING) {
 
-            printf("What would you like to do?\n");
+            char *addr_search;
+
             int chosen_operation;
-            while (input_integer(&chosen_operation) == false) printf("Invalid characters.\n");
+            while (input_integer(&chosen_operation) == false) printf(INVALID_INT_COMMAND_MSG);
             switch (chosen_operation) {
                 case 1:
                     entry_array = new_entry(entry_array, &array_len);
                     break;
                 case 2:
-                    print_entries("*", entry_array, array_len);
+                    printf("\nEnter website to search: ");
+                    addr_search = input_string();
+                    print_entries(addr_search, entry_array, array_len);
+                    free(addr_search);
                     break;
                 case 3:
+                    print_entries(PRINT_ALL, entry_array, array_len);
                     break;
                 case 4:
                     break;
                 case 5:
                     chosen_option = NO_OPTION;
+                    working_file = fopen(file_name, WRITE_BIN);
+                    if (working_file == NULL) {
+                        FILE_ERROR_MSG;
+                    }
                     write_entries(entry_array, array_len, working_file);
+                    fclose(working_file);
+                    free(file_name);
                     break;
                 case 6:
                     chosen_option = NO_OPTION;
-                    if (entry_array != NULL) free(entry_array);
+                    free(entry_array);
+                    free(file_name);
                     break;
                 default:
+                    printf(INVALID_CHOICE_COMMAND_MSG);
                     break;
             }
         }
-
-        fclose(working_file);
     }
 
     return 0;
-}
-
-ENTRY *init_entries() {
-    ENTRY *entry_array;
-    entry_array = malloc(sizeof(ENTRY));
-    MEM_CHECK(entry_array)
-    entry_array->addr_len = 0;
-    entry_array->pwd_len = 0;
-    return entry_array;
-}
-
-OPTION option_select(char *file_name, FILE **file) {
-
-    OPTION option = NO_OPTION;
-
-    int choice_main;
-    while (input_integer(&choice_main) == false) printf("Invalid characters.\n");
-
-    switch (choice_main) {
-        case 1:
-            *file = fopen(file_name, "rb");
-            if (*file == NULL) {
-                FILE_ERROR_MSG;
-            } else {
-                option = OPEN_EXISTING;
-            }
-            break;
-        case 2:
-            *file = fopen(file_name, "wb");
-            if (*file == NULL) {
-                FILE_ERROR_MSG;
-            } else {
-                option = CREATE_NEW;
-            }
-            break;
-        case 3:
-            option = QUIT;
-            printf("Quitting program...");
-            break;
-        default:
-            printf("Invalid choice.\n");
-            break;
-    }
-
-    return option;
 }
 
 bool delete_entry(char *addr, ENTRY *entry_array) {
@@ -183,15 +161,18 @@ bool delete_entry(char *addr, ENTRY *entry_array) {
 }
 
 char *encrypt(char *pwd, int pwd_len) {
-    char *master_buffer;
     int master_index = 0;
     int pwd_index = 0;
 
     printf("Enter master password: ");
+    char *master_buffer;
     master_buffer = input_string();
 
-    pwd = malloc(sizeof(char) * pwd_len);
-    MEM_CHECK(pwd)
+    pwd_len--;
+
+    pwd[pwd_len] = '\0';
+
+    printf("%s\n", pwd);
 
     while (pwd_index < pwd_len) {
 
@@ -199,32 +180,32 @@ char *encrypt(char *pwd, int pwd_len) {
 
         pwd_index++;
         master_index++;
-        if (master_index > (int) strlen(master_buffer)) master_index = 0;
+        if (master_index == (int) strlen(master_buffer)) master_index = 0;
     }
 
+    printf("%s\n", pwd);
+
     free(master_buffer);
+
     return pwd;
 }
 
 ENTRY *new_entry(ENTRY *entry_array, int *array_len) {
-    int i;
-    for (i = 0; (entry_array + i)->addr_len != 0; i++);
-    printf("\n\n%d\n\n", i);
-    entry_array = realloc(entry_array, sizeof(ENTRY) * (i + 2));
+    entry_array = realloc(entry_array, sizeof(ENTRY) * (*array_len + 1));
     MEM_CHECK(entry_array)
 
-    //(entry_array + (i + 1))->addr_len = 0;
-    //entry_array[i + 1] = entry_array[i];
-
     printf("Enter website address: ");
-    (entry_array + i)->addr = input_string();
-    (entry_array + i)->addr_len = (int) strlen((entry_array + i)->addr) + 1;
+    (entry_array + *array_len)->addr = input_string();
+    (entry_array + *array_len)->addr_len = (int) strlen((entry_array + *array_len)->addr) + 1;
 
     printf("Enter password: ");
     char *pwd_buffer;
     pwd_buffer = input_string();
-    (entry_array + i)->pwd_len = (int) strlen(pwd_buffer);
-    (entry_array + i)->pwd = encrypt(pwd_buffer, (entry_array + i)->pwd_len);
+
+    printf("%s\n", pwd_buffer);
+
+    (entry_array + *array_len)->pwd_len = (int) strlen(pwd_buffer) + 1;
+    (entry_array + *array_len)->pwd = encrypt(pwd_buffer, (entry_array + *array_len)->pwd_len);
 
     free(pwd_buffer);
 
@@ -258,46 +239,83 @@ ENTRY *read_entries(int *array_len, FILE *file) {
         (entry_array + i)->pwd = malloc(sizeof(char) * (entry_array + i)->pwd_len);
         MEM_CHECK((entry_array + i)->pwd)
         fread((entry_array + i)->pwd, sizeof(char), (entry_array + i)->pwd_len, file);
+        printf("%s\n", (entry_array + i)->pwd);
     }
 
-    entry_array = realloc(entry_array, sizeof(ENTRY) * (i + 1));
-    MEM_CHECK(entry_array)
-    (entry_array + i)->addr_len = 0;
-    (entry_array + i)->pwd_len = 0;
-
     return entry_array;
+}
+
+char *decrypt(char *pwd, int pwd_len, char *master) {
+    int master_index = 0;
+    int pwd_index = 0;
+
+    char *decrypted_pwd;
+    decrypted_pwd = malloc(sizeof(char) * pwd_len);
+    MEM_CHECK(decrypted_pwd)
+
+    pwd_len--;
+
+    decrypted_pwd[pwd_len] = '\0';
+
+    while (pwd_index < pwd_len) {
+
+        decrypted_pwd[pwd_index] = (char) (pwd[pwd_index] ^ master[master_index]);
+
+        pwd_index++;
+        master_index++;
+        if (master_index == (int) strlen(master)) master_index = 0;
+    }
+
+    return decrypted_pwd;
 }
 
 void print_entries(char *addr, ENTRY *entry_array, int array_len) {
 
     if (array_len == 0) {
-        printf("No passwords stored.\n");
+        printf(NO_PWD_MSG);
     } else {
         int longest_index = strlen(NMR);
         int longest_addr = strlen(ADDR);
         int longest_pwd = strlen(PWD);
 
+        int index = 0;
+
         for (int i = 0; i < array_len; i++) {
-
-            int digits = 0;
-            int temp_index = i;
-            while (temp_index != 0) {
-                temp_index = temp_index / 10;
-                digits++;
-            }
-            if (digits > longest_index) longest_index = digits;
-            digits = 0;
-
+            if (strcmp(PRINT_ALL, addr) == 0 || strcmp((entry_array + i)->addr, addr) == 0) index++;
             if ((entry_array + i)->addr_len > longest_addr) longest_addr = (entry_array + i)->addr_len;
             if ((entry_array + i)->pwd_len > longest_pwd) longest_pwd = (entry_array + i)->pwd_len;
         }
 
+        /*int digits = 0;
+        int temp_index = index;
+        while (temp_index != 0) {
+            temp_index = temp_index / 10;
+            digits++;
+        }
+        if (digits > longest_index) longest_index = digits;
+        digits = 0;*/
+
+        printf("Enter master password: ");
+        char *master_buffer;
+        master_buffer = input_string();
+
         printf("%-*s | %-*s | %-*s\n", longest_index, NMR, longest_addr, ADDR, longest_pwd, PWD);
 
+        index = 0;
         for (int i = 0; i < array_len; i++) {
-            printf("%*d | %*s | \n", longest_index, i + 1, longest_addr, (entry_array + i)->addr);
-            printf("%d - %d\n", (entry_array + i)->addr_len, (entry_array + i)->pwd_len);
+            if (strcmp(PRINT_ALL, addr) == 0 || strcmp((entry_array + i)->addr, addr) == 0) {
+                index++;
+
+                char *decrypted_pwd;
+                decrypted_pwd = decrypt((entry_array + i)->pwd, (entry_array + i)->pwd_len, master_buffer);
+
+                printf("%*d | %*s | %*s\n", longest_index, index, longest_addr, (entry_array + i)->addr, longest_pwd, decrypted_pwd);
+
+                free(decrypted_pwd);
+            }
         }
+
+        free(master_buffer);
     }
 }
 
@@ -355,7 +373,7 @@ char *input_string() {
 
         if (strlen(string) == 0) {
             free(string);
-            printf("Enter valid file name: ");
+            printf(EMPTY_STRING_MSG);
         } else {
             success = true;
         }
